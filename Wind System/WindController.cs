216b38b28c1c0febe10c45bef9b9a845039c2cbd
B3NII <89522330+B3N11@users.noreply.github.com>
2021-08-26 02:48:14 +0200

    Created Wind System

---
+++ Wind System/WindController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Environment
+{
+    namespace NaturalEffects
+    {
+        public class WindController : MonoBehaviour
+        {
+            #region System description
+            /* BASIC, ARCADE PHYSICS BASED WIND CODE
+             * 
+             * HOW DOES IT WORK: The GameObject with this script has an Animation Curve that determines the "airflow", the speed of the wind. The
+             * vertical axis of the graph represents the max value (speed) of the wind. As the position of this Speed Curve changes by the time,
+             * the speed of the airflow changes too. This airflow represents a force that will be applied to the GameObjects with the Wind Target component.
+             * 
+             * HOW TO USE IT: Create an empty GameObject (suggested to name it something like "Wind Controller/Handler")! Add this script to the Wind Controller!
+             * The position of this GameObject doesn't matter.
+             * The field "direction" stands for the (surprisingly) direction of the wind. It is important, because the force will be applied from this direction. 
+             * The direction of the GameObject is calculated by its rotation! Set up the Speed Curve by choosing a pre-created curve, or create your own.
+             * Set the max speed so as the change speed. Then, attach the Wind Target and (optional) the Wind Agent to the right GameObjects. Choose the
+             * appropriate script that you need. Here are their functions:
+             * 
+             * WORKING OF THE WIND TARGET: Wind Target should be added to all GameObjects you want to be affected by the wind. It has two types:
+             * STATIC and DYNAMIC
+             * In the case of STATIC, you don't have to do anything after adding the script and choosing its Target Type. This scripts needs a reference to the
+             * Wind Controller in the scene and it will get the informations in every FixedUpdate call from it. The following variables are passed:
+             * direction and windSpeed. Static Wind Target simply applies a force with the current speed of the wind (windSpeed) continuosly.
+             * In the case of DYNAMIC, you will also need a different GameObject with the Wind Agent script attached to it. Only thing you must do is selecting
+             * the type as DYNAMIC on the Target. This type of Target is recommended to be used on Player and other kind of moving character.
+             * 
+             * WORKING OF THE WIND AGENT: If you set your Target as a DYNAMIC Target, you need to create a GameObject called Wind Agent. What this object does
+             * is shooting RayCasts forward and checking what it hit. It will only apply the force to the given Target, if the RayCast hits it. This means that,
+             * if the Target is behind a wall or other kind of cover, the RayCast will hit the cover, the "airflow breaks" and the Target won't be affected by
+             * the wind as long as it doesn't leave the cover and contacts the RayCast again. The distance between the Agent and the Target is also very important.
+             * If it is too high, the Target won't be affected on more opened areas (such as cornfields). If it is too low, not many objects will fit in the space
+             * between the Target and the Agent. There will be hardly any cover, that the Target can use to avoid wind. Of course, this part is all about personal
+             * preference and the type of your game.
+             * 
+             * Desired structure of a Dynamic WindTarget:
+             * 
+             * Player
+             *  - Colliders
+             *      - Body_Collider
+             * 
+             * The Agent will look for the parent of the parent of the collider it hit. If it hits the Body_Collider GameObject, it will look for the parent
+             * (Player) of its parent (Colliders). This can be changed in the WindAgent script in the line of 72 (void ShootRayCast() method).
+             * 
+             * WIND PARTICLE FEATURE: You have the opportunity to visualise the wind by attaching a "follow" script to a particle system or to its parent.
+             * You can determine the distance from the player.
+             * 
+             * Created: 2020 october
+             *          in Unity 2020.1.9f1 version
+             * System version: v.1.1.0
+             * 
+             * Any questions or problems? Maybe want to share your opinion or your suggestion? Contact with me: ********@gmail.com
+             */
+            #endregion
+
+            #region Fields
+            //Properties
+            [Header("Wind Properties")]
+            [Tooltip("Defines the changing method of the wind speed. Horizontal axis represents the time, vertical sets the current speed in percentage (0 - 100 %).")]
+            public AnimationCurve speedCurve = AnimationCurve.EaseInOut(0f, 0f, 1f, 1f);
+
+            [NonSerialized] public Vector3 direction = Vector3.zero;    //Represents the direction where the airflow is coming from
+
+            [Tooltip("Set it by knowing that this represents the amount of force, that will be applied to the objects affected by wind (the Targets).")]
+            public float maxSpeed = 50f;
+
+            [Tooltip("Keep it between 0 and 1. The closer the value to 1, the faster the airflow changes. It also determines the amount of steps the curve position is moving with. The less the value the more precious the output on the curve.")]
+            public float changeSpeed = 0.01f;    //Speed of airflow change (KEEP IT BETWEEN 0 AND 1!!)
+
+            [Tooltip("By setting this, you can turn on and off the wind. If you want to change it by code, you can access it like: \n\nmyWindController.enableWind = false;")]
+            public bool enableWind = true;
+
+            private float curvePosition;    //Changes over time, represents the X on the horizontal axis of the curve
+
+            //Effects
+            [Header("Visual Effects")]
+            [Tooltip("Only DIRECTIONAL WindZones will be handled in this list.")]
+            public List<WindZone> windZones = new List<WindZone>();
+
+            //Output
+            [Header("Output")]
+            [Tooltip("Represents the current speed of the wind. It changes over time and is calculated from the Speed Curve.")]
+            public float windSpeed = 0f;    //Represents the current value of the Speed Curve, the current "speed of the air"
+
+            public float speedInMph = 0f;
+
+            //Editor specials
+            [Header("Editor Specials")]
+            [Tooltip("Draws a ray to the direction of the wind with the length of the wind speed.")]
+            public bool showWindSpeed = true;
+            #endregion
+
+            #region Functions
+            private void FixedUpdate()
+            {
+                UpdateRotation();
+                CalculateWindSpeed();
+                UpdateOutput();
+            }
+
+            private void UpdateRotation()
+            {
+                direction = transform.forward;
+            }
+
+            private void CalculateWindSpeed()
+            {
+                //Calculating the current speed of the airflow
+                windSpeed = speedCurve.Evaluate(curvePosition) * maxSpeed;
+
+                //Moving the position of the curve and keeping it between 0 and 1. Then looping it
+                curvePosition += changeSpeed;
+                curvePosition = Mathf.Clamp01(curvePosition);
+                if (curvePosition == 1) { curvePosition = 0; }
+
+                //Draw the current wind speed it it is allowed
+                if (showWindSpeed) { Debug.DrawRay(transform.position, transform.forward * windSpeed, Color.red); }
+            }
+
+            private void UpdateOutput()
+            {
+
+            }
+
+            private void SetWindZones()
+            {
+                foreach (WindZone actual in windZones)
+                {
+                    if (actual.mode == WindZoneMode.Directional)
+                    {
+                        actual.transform.rotation = Quaternion.Euler(direction);
+                    }
+                }
+            }
+            #endregion
+        }
+    }
+}
---
+++ Wind System/WindTarget.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using UnityEngine;
+
+namespace Environment
+{
+    namespace NaturalEffects
+    {
+        [RequireComponent(typeof(Rigidbody))]
+        public class WindTarget : MonoBehaviour
+        {
+            #region Fields
+            //Main Properties
+            [Header("Properties")]
+            [Tooltip("Represents the type of the Wind Target. Set it to STATIC, if you want your object to be continously effected by the wind. Set it to DYNAMIC, if your object will move (for example: your player) or you don't want it to be effected by the wind over time. Wind Agent required if you use DYNAMIC Target.")]
+            public TargetType targetType;
+
+            [Tooltip("Defines how the wind pushes the target.\n\nWIND SPEED: Pushes the plane by the actual wind speed, independently from the angle between its forward vector and the wind.\n\nSIDE FORCE: Pushes the target with the force, that is calculated from the angle between the forward vector and the wind.")]
+            public MovingMethods moveBy;
+
+            [Tooltip("Defines on which axis the target should be rotated towards the wind.")]
+            public Vector3 rotationAxis = Vector3.zero;
+
+            //Wind Effect Properties
+            [Header("Wind Effect Properties")]
+            [Tooltip("Enables for the wind to push the target object forward (to the wind direction).")]
+            public bool enableMove = true;
+
+            [Tooltip("Enables for the wind to rotate the target object by adding some torque to it. It rotates the target until it is facing to the direction of the wind.")]
+            public bool enableRotate = true;
+
+            //Components
+            protected Rigidbody rb;
+
+            //Output
+            [Header("Output")]
+            [Tooltip("Defines the method of the Side Force calculation. SF is a force that is calculated based on the angle between the target and the wind direcion (target.transform.forward, wind.direction). NOTE, that this is not the force, that is applied to the target. This force is used for rotating towards the wind direction, if it is enabled. Of yourse, you can use it as you want.\n\nPERPENDICULAR: The closer the angle to 90°, the bigger the force. (Example for use: Airplane is rotating towards the wind by this Side Force. If the plane is perpendicular to the wind, the wind will rotate it faster.\n\nPARALLEL: The more parallel the two vectors are, the bigger the force. (Example for use: Sail of the ship (its forward vector) is parallel to the wind. The ship will go faster.)")]
+            public SideForceType sideForceCalculationMethod;
+
+            [Tooltip("Stores the angle in degrees between the target and the wind direction. In the case of Perpendicular SF calculation, it goes from -180° to 180°. In the case of Parallel SF calculation, it goes from 0° to 180°.")]
+            public float angleBetweenTargetAndWind = 0f;
+
+            [Tooltip("Stores a value that is calculated from the angle between the target and the wind direction.\nThis value can be used as you wish, for example to controll a ship if the sail is perfectly parallel to the wind.")]
+            public float sideForcePercentage = 0f;
+
+            [Tooltip("The Side Force itself. Its calculation is based on the method you chose, the maximum wind speed and the angle between the target and the wind.")]
+            public float sideForce = 0f;
+
+            //References
+            [Header("References")]
+            public WindController windController;
+
+            [Tooltip("The SF is calculated from the angle between this GameObject (forward vector) and the wind direction.\n\nFor example: If you are controlling a ship with the wind, this GameObject should be the sail of the ship. The angle between the sail and the wind will define the force that will be applied to the target.\n\nAnother example: If you are flying an airplane, this GameObject should be the same as the GameObject of this script (the airplane), because the force will be calculated from the angle between the plane and the wind.")]
+            public Transform referenceForAngleCalculation;
+            #endregion
+
+            #region Functions
+            private void Start()
+            {
+                rb = GetComponent<Rigidbody>();
+            }
+
+            private void FixedUpdate()
+            {
+                CheckType();
+            }
+
+            private void CheckType()
+            {
+                if (targetType == TargetType.Static && windController.enableWind) { HandleWindEffects(); }
+            }
+
+            public void HandleWindEffects()
+            {
+                HandleSideForceMethod();
+                if (enableMove) { HandleMoveTypes(); }
+                if (enableRotate) { RotateByForce(); }
+            }
+
+            private void HandleSideForceMethod()
+            {
+                switch (sideForceCalculationMethod)
+                {
+                    case SideForceType.Perpendicular:
+                        CalculatePerpendicular();
+                        break;
+
+                    case SideForceType.Parallel:
+                        CalculateParallel();
+                        break;
+                }
+            }
+
+            private void CalculatePerpendicular()
+            {
+                //Getting the angle between the wind direction and transform.forward
+                float angle = Vector3.SignedAngle(referenceForAngleCalculation.forward, windController.direction, Vector3.up);
+                float rad = angle * Mathf.Deg2Rad;
+
+                //Converting the angle to a value between -1 and 1 by getting its SIN
+                //Defines the windspeed in percentage and the direction of the rotation
+                float percentage = Mathf.Sin(rad);
+
+                //Setting the values
+                angleBetweenTargetAndWind = angle;
+                sideForcePercentage = percentage;
+                sideForce = windController.windSpeed * percentage;
+            }
+
+            private void CalculateParallel()
+            {
+                //Getting the angle between the wind direction and transform.forward
+                float angle = Vector3.Angle(referenceForAngleCalculation.forward, windController.direction);
+                float rad = angle * Mathf.Deg2Rad;
+
+                //Converting the angle to a value between -1 and 1 by getting its SIN
+                //Defines the windspeed in percentage and the direction of the rotation
+                float percentage = Mathf.Cos(rad);
+
+                //Setting the values
+                angleBetweenTargetAndWind = angle;
+                sideForcePercentage = percentage;
+                sideForce = windController.windSpeed * percentage;
+            }
+
+            private void HandleMoveTypes()
+            {
+                switch (moveBy)
+                {
+                    case MovingMethods.WindSpeed:
+                        MoveByWindSpeed();
+                        break;
+
+                    case MovingMethods.SideForce:
+                        MoveBySideForce();
+                        break;
+                }
+            }
+
+            private void MoveByWindSpeed()
+            {
+                rb.AddForce(windController.direction * windController.windSpeed);
+            }
+
+            private void MoveBySideForce()
+            {
+                rb.AddForce(windController.direction * sideForce);
+            }
+
+            private void RotateByForce()
+            {
+                rb.AddTorque(rotationAxis * sideForce);
+            }
+            #endregion
+        }
+
+        public enum TargetType
+        {
+            Static,
+            Dynamic
+        }
+
+        public enum SideForceType
+        {
+            Perpendicular,
+            Parallel
+        }
+
+        public enum MovingMethods
+        {
+            WindSpeed,
+            SideForce
+        }
+    }
+}
